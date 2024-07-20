5c221ab938f87450e94a89d635631476ef043698
Caspar Nettelbladt <caspar.n@gmail.com>
2024-07-20 23:09:33 +0200

    Enable Copilot for specific filetypes

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -38,6 +38,17 @@ return {
             dismiss = "<C-]>",
           },
         },
+        filetypes = {
+          yaml = true,
+          markdown = true,
+          help = true,
+          gitcommit = true,
+          gitrebase = true,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
       }
     end,
   },
