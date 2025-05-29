a4c6765d6572ee6cfeec6d93bf305c89e58452d1
Caspar Nettelbladt <caspar.n@gmail.com>
2025-05-29 13:40:09 +0200

    comment out pyright settings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -51,16 +51,16 @@ return {
         },
         pyright = {
           settings = {
-            pyright = {
-              -- Using Ruff's import organizer
-              disableOrganizeImports = true,
-            },
-            python = {
-              -- analysis = {
-              --   -- Ignore all files for analysis to exclusively use Ruff for linting
-              --   ignore = { "*" },
-              -- },
-            },
+            -- pyright = {
+            --   -- Using Ruff's import organizer
+            --   disableOrganizeImports = true,
+            -- },
+            -- python = {
+            --   analysis = {
+            --     -- Ignore all files for analysis to exclusively use Ruff for linting
+            --     ignore = { "*" },
+            --   },
+            -- },
           },
         },
         bashls = true,
