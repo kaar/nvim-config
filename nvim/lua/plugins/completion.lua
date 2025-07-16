6f7f5847cb5fda55c3980df41dda9d3457202de6
Caspar Nettelbladt <caspar.n@gmail.com>
2025-07-16 16:14:33 +0200

    Moved supermaven config
    Didn't work when put into completion.lua

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -18,16 +18,6 @@ return {
 
     -- https://supermaven.com, replacement for copilot
     -- https://github.com/supermaven-inc/supermaven-nvim
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<C-e>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-j>",
-        },
-      })
-    end,
   }
 }
 
