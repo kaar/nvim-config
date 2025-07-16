99b2775edbda72ef41b23ab92e6146d58130d1c9
Caspar Nettelbladt <caspar.n@gmail.com>
2025-07-16 15:30:02 +0200

    Add supermaven as a replacement for copilot
    Add link to supermaven-nvim plugin

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -15,7 +15,20 @@ return {
     --   require "custom.completion"
     -- end,
     -- TODO: I should have completion stuff somewhere
-  },
+
+    -- https://supermaven.com, replacement for copilot
+    -- https://github.com/supermaven-inc/supermaven-nvim
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-e>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+      })
+    end,
+  }
 }
 
 -- require "custom.snippets"
