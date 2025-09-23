09661d02a3b6b89b8920541c86dc1cd2c886577a
Caspar Nettelbladt <caspar.n@gmail.com>
2025-09-23 14:29:58 +0200

    Configure HTML LSP with embedded language support

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -99,7 +99,18 @@ return {
         -- svelte = true,
         -- templ = true,
         cssls = true,
-        html = true,
+        html = {
+          cmd = { "vscode-html-language-server", "--stdio" },
+          filetypes = { "html", "templ" },
+          init_options = {
+            configurationSection = { "html", "css", "javascript" },
+            embeddedLanguages = {
+              css = true,
+              javascript = true
+            },
+            provideFormatter = true
+          },
+        },
         ts_ls = true,
         -- biome = true,
 
