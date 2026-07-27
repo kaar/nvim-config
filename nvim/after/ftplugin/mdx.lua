be98cda5190dab30465a06336be801e500cae8d4
Caspar Nettelbladt <caspar.n@gmail.com>
2026-07-27 12:19:53 +0200

    Add MDX filetype configuration for line wrapping

---
+++ nvim/after/ftplugin/mdx.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt_local
+
+opt.wrap = true      -- Wrap lines at the end of the screen
+opt.linebreak = true -- Break at word boundaries
+opt.textwidth = 0    -- Don't auto-insert line breaks
